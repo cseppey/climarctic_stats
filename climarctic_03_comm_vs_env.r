2ec10cb7b44b4b3748b7f7dd3830b2b6e463f738
cseppey <christophe.seppey@uit.no>
2020-02-18 08:39:34 +0100

    03_comm_vs_env   remove nutrient and gaz variables   remove transfo   add NMDS and envfit test   add clr without low sequences samples   procrust analysis on shared samples (NMDS)

--- climarctic_03_comm_vs_env.r
+++ climarctic_03_comm_vs_env.r
@@ -3,7 +3,6 @@
 #####
 
 rm(list=ls())
-gc()
 
 require(foreach)
 require(doSNOW) # e.g. makeSOCKcluster()
@@ -24,10 +23,10 @@ dir.create(dir_cve, showWarnings=F)
 file <- paste0(dir_save, '00_lst_comm.Rdata')
 load(file)
 
-# file <- paste0(dir_save, '03_lst_comm.Rdata')
-# if(file.exists(file)){
-#   load(file)
-# }
+file <- paste0(dir_save, '03_lst_comm.Rdata')
+if(file.exists(file)){
+  load(file)
+}
 
 #########
 permu <- 1000
@@ -37,197 +36,343 @@ permu <- 1000
 fact_3 <- c('site','moisture','depth')
 
 # loop on communities
-for(i in names(lst_comm)){
-  stat2 <- NULL
-  for(j in 'rrf2'){#names(lst_comm[[i]])){
-
-    if(is.null(lst_comm[[i]][[j]])){
-      next
-    }
+lsts <- NULL
+for(h in c('clr','clr2')){
+  for(i in names(lst_comm)) {
     
-    env <- lst_comm[[i]][[j]]$env
-    env <- env[,c('site','moisture','depth','plot_in_moist_in_site',
-                  'pH','C_N','S','NO3','NH4','P_labile',
-                  'silt','clay','rh','om',"CH4","CO2","N2O")]
+    mr <- lst_comm[[i]][[h]]$mr
+    env <- lst_comm[[i]][[h]]$env
     
-    # transfo ####
-    print(paste(i, j, 'transfo'))
-    
-    mr <- lst_comm[[i]][[j]]$mr
-    mr <- mr[rowSums(mr) != 0,colSums(mr) != 0]
     env <- env[row.names(mr),]
+    env <- env[,c('site','moisture','depth','plot_in_moist_in_site',
+                  'pH','C','N','C_N','S','silt','clay','rh','om')]
     
-    # transfo <- c('raw','hell','log')
-    transfo <- c('log')
+    # ordination ####
+    print(paste(i, 'model'))
     
-    if('lst_trsf_bc' %in% names(lst_comm[[i]][[j]]) == F){
-      lst_trsf <- foreach(k=transfo) %dopar% {
-        if(k == 'raw'){
-          return(list(mr=mr, bc=vegdist(mr)))
-        } else {
-          m <- decostand(mr, k)
-          return(list(mr=m, bc=vegdist(m)))
-        }
-      }
-      names(lst_trsf) <- transfo
-      lst_trsf <- append(lst_trsf, list(env))
-      names(lst_trsf)[length(lst_trsf)] <- 'env'
-      
-      lst_comm[[i]][[j]][['lst_trsf_bc']] <- lst_trsf
+    e <- na.omit(env)
+    is.fact <- sapply(e, is.factor)
+    m <- mr[row.names(e),]
+    m <- m[,colSums(m) != 0]
+    
+    # RDA ----
+    f <- formula(paste0('m~depth+', paste(names(e)[is.fact == F], collapse='+'),
+                            '+Condition(site)', collapse=''))
+    rda_full <- capscale(f, data=e, scale=T)
+    
+    lst_rda <- list(full=list(e=e, m=m, f=f, mod=rda_full))
+    
+    # retreive a more parcimonious model ---
+    ### note: the parcimonious model did not depend of the order of the variable in the input model
+    
+    print('parci')
+    
+    file <- paste0(dir_save, '03_rda_parsi_', i, '_', h, '.Rdata')
+    if(file.exists(file) == F){
+      set.seed(0)
+      rda_parci <- ordistep(capscale(m~depth+rh+Condition(site), data=e, scale=T),
+                            formula(rda_full), parallel=4, trace=F)
+      save(rda_parci, file=file)
+    } else {
+      load(file)
     }
     
-    # model ####
-    print(paste(i, j, 'model'))
+    v_parci <- attributes(rda_parci$terminfo$terms)$term.labels
     
-    env <- lst_comm[[i]][[j]]$lst_trsf_bc$env
+    e <- na.omit(env[,v_parci])
     
-    res_tst <- foreach(k=transfo, .verbose=T) %dopar% {
-      
-      bc <- lst_comm[[i]][[j]]$lst_trsf_bc[[k]]$bc
-      
-      e <- na.omit(env)
-      is.fact <- sapply(e, is.factor)
-      b <- as.dist(as.matrix(bc)[row.names(e),row.names(e)])
-      
-      if(nrow(e) >= ncol(e)-1){
-      
-        # rda
-        formu <- formula(paste0('b~depth+', paste(names(e)[is.fact == F], collapse='+'), '+Condition(site)', collapse=''))
-        rda <- capscale(formu, data=e, scale=T)
+    m <- mr[row.names(e),]
+    m <- m[,colSums(m) != 0]
+    
+    f <- formula(paste(c('m~depth+rh', v_parci[grepl('depth|rh|site', v_parci) == F],
+                             'Condition(site)'), collapse='+'))
+    
+    rda_parci <- capscale(f, data=e, scale=T)
+  
+    lst_rda[['parci']] <- list(e=e, m=m, f=f, mod=rda_parci)
+    
+    # retreive info on the two models ---
+    
+    file <- paste0(dir_save, '03_lst_ordi_', i, '_', h, '.Rdata')
+    if(file.exists(file) == F){
+    
+      lst_ordi <- NULL
+      for(j in 1:2){
         
-        # retreive a more parcimonious model
-        ### note: the parcimonious model did not depend of the order of the variable in the input model
-        set.seed(0)
-        rda_parci <- ordistep(capscale(b~depth+rh+Condition(site), data=e, scale=T), formula(rda), trace=F)
-        v <- attributes(rda_parci$terminfo$terms)$term.labels
+        print(names(lst_rda)[j])
         
-        e <- na.omit(env[,v])
-        b <- as.dist(as.matrix(bc)[row.names(e),row.names(e)])
-        rda_parci <- capscale(formula(paste(c('b~depth+rh', names(e)[grepl('depth|rh|site', names(e)) == F],
-                                              'Condition(site)'), collapse='+')), data=e)
+        e   <- lst_rda[[j]]$e
+        f   <- lst_rda[[j]]$f
+        mod <- lst_rda[[j]]$mod
         
-        # graf
-        pdf(paste0(dir_cve, 'RDA_', i, '_', j, '_', k, '.pdf'), width=10, height=7)
-        layout(matrix(c(1,3,4, 1,5,6, 0,2,2), nrow=3, byrow=T), width=c(0.2,1,1), height=c(1,1,0.3))
+        v <- rev(rev(attributes(mod$terminfo$terms)$term.labels)[-1])
         
-        stat <- NULL
-        for(l in list(rda, rda_parci)){
+        stat <- as.data.frame(foreach(k=v, .combine=rbind) %dopar% {
           
-          par(mar=rep(0,4), oma=rep(1,4))
+          m <- lst_rda[[j]]$m
+          mod <- capscale(eval(parse(text=paste(as.character(f)[c(2,1,3)], collapse=''))), 
+                          data=e, scale=T) # some fuck with the foreach environment 
+                                           # but output are the same then if tested sequencially
           
-          # coord prep
-          s <- summary(l)
+          set.seed(0)
           
-          site <- s$sites[,1:2]
+          upd <- update(mod, as.formula(paste('.~.-', k)))
+          pv1 <- signif(anova(mod, upd, parallel=4, permutations=permu)$`Pr(>F)`[2], 2)
+          va1 <- RsquareAdj(mod)[[1]] - RsquareAdj(upd)[[1]] # no R2 adj because Conditon(site)
           
-          v <- unlist(strsplit(gsub(' ', '', as.character(s$call$formula[[3]][2])), '+', fixed=T))
-          var <- s$biplot
-          var <- var[row.names(var) %in% v,1:2]
+          rda1 <- capscale(as.formula(paste('m~', k, '+Condition(site)')), data=e, scale=T)
+          pv2 <- signif(anova(rda1, parallel=4, permutations=permu)$`Pr(>F)`[1], 2)
+          va2 <- RsquareAdj(rda1)[[1]]
           
-          e <- env[row.names(site),]
-          b <- as.dist(as.matrix(bc)[row.names(e),row.names(e)])
-          
-          coord <- rbind(site, var)
-          
-          # axis
-          vari <- s$cont$importance[2,1:2]
-          vari <- paste0(names(vari), ' ', signif(vari*100,2), '%')
-          for(m in 2:1){
-            plot.new()
-            text(0.5,0.5, labels=vari[m], srt=ifelse(m == 1, 0, 90))
-          }
-          
-          # ordinations
-          for(m in seq_along(fact_3)){
-            
-            mn <- fact_3[m]
-            
-            plot(site, xlim=range(coord[,1], na.rm=T), ylim=range(coord[,2], na.rm=T), xaxt='n', yaxt='n',
-                 col=lst_palev[[mn]]$pal[e[[mn]]], pch=NA)
-            if(m != 1){
-              axis(1)
-            }
-            if(m != 2){
-              axis(2)
-            }
-            
-            ordispider(site, factor(paste(e$depth, e$plot_in_moist_in_site)), col='grey80')
-            
-            text(site, labels=row.names(site), col=lst_palev[[mn]]$pal[e[[mn]]])
-            
-            abline(v=0,h=0,lty=3)
-            
-            arrows(0,0,var[,1],var[,2], length=0, lty=2, col='grey70')
-            text(var[,1:2], labels=paste(row.names(var)))
-            
-          }
-          
-          # legend ---
-          plot.new()
-          
-          # factors colors
-          legend(0.5,0.5, legend=c(unlist(sapply(lst_palev, '[[', 2))), bty='n', xjust=0.5, yjust=0.5,
-                 pch=19, col=unlist(sapply(lst_palev, '[[', 1)))
-          
-          # test each variable
-          vtt <- rev(rev(attributes(l$terms)$term.labels)[-1])
-          pvs <- matrix(NA, nrow=ncol(env)-3, ncol=4, dimnames=list(names(env)[-c(1,2,4)], c('pv_strict','va_strict', 'pv_all','va_all')))
-          for(m in vtt){
-            set.seed(0)
-            
-            upd <- update(l, as.formula(paste('.~.-', m)))
-            pv1 <- signif(anova(l, upd, parallel=4, permutations=permu)$`Pr(>F)`[2], 2)
-            va1 <- RsquareAdj(l)[[1]] - RsquareAdj(upd)[[1]]
-            
-            rda1 <- capscale(as.formula(paste('b~', m)), data=e, scale=T)
-            pv2 <- signif(anova(rda1, permutations=permu)$`Pr(>F)`[1], 2)
-            va2 <- RsquareAdj(rda1)[[1]] 
-            
-            pvs[m,] <- c(pv1, va1, pv2, va2)
-          }
-          
-          ### note: the AIC did not depend on the order of the variables in the model
-          ### note: the RsqrtAdj is not available because of the Condition(site)
-          stat <- cbind(stat, rbind(pvs, info=c(extractAIC(l)[2], RsquareAdj(l)[1], nrow(site),
-                                                length(unlist(strsplit(as.character(l$call$formula[[3]][2]), '+', fixed=T))))))
-          
-        }
+          return(c(pv1, va1, pv2, va2))
+        })
         
-        dev.off()
+        dimnames(stat) <- list(v, c('pv1','va1','pv2','va2'))
         
-        st <- apply(stat, 2, as.numeric)
-        row.names(st) <- c(row.names(pvs), 'AIC')
+        #---
+        s <- summary(mod)
         
-        return(st)      
-      } else {
-        return(dim(e))
+        lst_ordi[[j]] <- list(site       = s$sites[,1:2],
+                              var        = s$biplot[row.names(s$biplot) %in% names(is.fact)[is.fact == F],1:2],
+                              axes_names = paste('RDA; var =', signif(s$cont$importance[2,1:2], 2)),
+                              stat       = stat)
       }
+      
+      names(lst_ordi) <- names(lst_rda)
+      
+      
+      # NMDS ----
+      nmds <- metaMDS(mr, dist='euc') # aitchinson distance = euclydian dist on CLR
+      
+      # test variables of parci
+      set.seed(0)
+      
+      envfit <- envfit(nmds, env[,v_parci], na.rm=T)
+      
+      var_env <- lapply(envfit[1:2], function(x) matrix(unlist(x[c('pvals','r')]), ncol=2, 
+                                                        dimnames=list(names(x$r), c('pv2','va2'))))
+      var_env <- rbind(var_env[[2]],var_env[[1]])
+      
+      lst_ordi[['nmds']] <- list(site       = nmds$points,
+                                 var        = envfit$vectors$arrows,
+                                 axes_names = c('NMDS1','NMDS2'),
+                                 stat       = var_env)
+      
+      save(lst_ordi, file=file)
+      
+    } else {
+      load(file)
     }
     
-    names(res_tst) <- transfo
-    stat2[[j]] <- res_tst
+    # graf ----
+    print('graf')
+    
+    pdf(paste0(dir_cve, 'ordination', i, '_', h, '.pdf'), width=10, height=7)
+    layout(matrix(c(1,3,4, 1,5,6, 0,2,2), nrow=3, byrow=T), width=c(0.2,1,1), height=c(1,1,0.3))
+    
+    for(j in lst_ordi){
+      
+      # axes
+      par(mar=rep(0,4), oma=c(1,1,3,3))
+      for(k in 2:1){
+        plot.new()
+        text(0.5,0.5, labels=j$axes_names[k], srt=ifelse(k == 1, 0, 90))
+      }
+      
+      # ordinations
+      site <- j$site
+      var <- j$var
+      
+      coord <- rbind(site, var)
+      
+      for(k in seq_along(fact_3)){
+        
+        fact <- fact_3[k]
+        
+        env_in_ordi <- env[row.names(env) %in% row.names(j$site),]
+        
+        pal <- lst_palev[[fact]]$pal[env_in_ordi[[fact]]]
+        
+        #---
+        plot(j$site, xlim=range(rbind(coord[,1])), ylim=range(coord[,2]), xaxt='n', yaxt='n',
+             col=pal, pch=NA)
+        
+        if(k > 1){
+          axis(1)
+        }
+        
+        if(k %% 2 == 1){
+          axis(2)
+        }
+        
+        ordispider(site, env_in_ordi$plot_in_moist_in_site, col='grey80')
+        
+        ls <- lst_comm[[i]][[h]]$env$low_seq[row.names(lst_comm[[i]][[h]]$env) %in% row.names(site)]
+        points(site[ls,], pch=19, col=2)
+        
+        text(site, labels=row.names(site), col=pal)
+        
+        abline(v=0, h=0, lty=3)
+        
+        #---
+        
+        mult <- min(abs(sapply(as.data.frame(site), range)))
+        v2 <- var*mult
+        
+        arrows(0,0,v2[,1],v2[,2], length=0, lty=3, col='red')
 
-  }
-  
-  # out stat
-  file <- paste0(dir_cve, 'stat_', i, '.csv')
-  if(file.exists(file)){
-    file.remove(file)
-  }
-  
-  for(k in names(stat2)){
-    write.table(k, file, T)
-    for(l in names(stat2[[k]])){
-      write.table(l, file, T)      
-      write.table(stat2[[k]][[l]], file, T)
+        if(k < 3){        
+          axis(3, at=seq(-mult, mult, length.out=5), labels=seq(-1, 1, length.out=5), col=2)
+        }
+        
+        if(k > 1){
+          axis(4, at=seq(-mult, mult, length.out=5), labels=seq(-1, 1, length.out=5), col=2)
+        }
+        
+        text(v2[,1:2], labels=paste(row.names(v2)), col=2)
+        
+      }
+      
+      # legend ---
+      plot.new()
+      
+      # factors colors
+      legend(0.5,0.5, legend=c(unlist(sapply(lst_palev, '[[', 2))), bty='n', xjust=0.5, yjust=0.5,
+             pch=19, col=unlist(sapply(lst_palev, '[[', 1)))
+      
     }
     
+    dev.off()
+  
+    ###
+    lsts[[h]][[i]] <- list(lst_rda=lst_rda, lst_ordi=lst_ordi)
   }
+}
+
+file <- paste0(dir_save, '03_lsts.Rdata')
+save(lsts, file=file)
+load(file)
+
+#---
+file <- paste0(dir_cve, 'stat_ordi.csv')
+if(file.exists(file)){
+  file.remove(file)
+}
+
+for(i in names(lsts)){
+  for(j in names(lsts[[i]])){
+    for(k in names(lsts[[i]][[j]]$lst_ordi)){
+      write.table(c(i,j,k), file, T, F, '\t')
+      write.table(lsts[[i]][[j]]$lst_ordi[[k]]$stat, file, T, F, '\t')
+    }
+  }
+}
+
+# procrust rotation on the NMDS with common samples ####
+
+# find the samples found in all datasets
+smp_4 <- names(which(table(unlist(lapply(lst_comm, function(x) row.names(x$clr$mr)))) == 4))
+
+# NMDS ---
+lst_nmds_4 <- lapply(lst_comm, function(x) {
+  m <- x$clr$mr[smp_4,]
+  m <- m[,colSums(m) != 0]
+  return(metaMDS(m, distance='euc'))
+})
+
+# grafs
+pdf(paste0(dir_cve, 'nmds_4_', i, '.pdf'), width=10, height=7)
+layout(matrix(c(1,3,4, 1,5,6, 0,2,2), nrow=3, byrow=T), width=c(0.2,1,1), height=c(1,1,0.3))
+
+for(i in names(lst_nmds_4)){
+  
+  # axes
+  par(mar=rep(0,4), oma=c(1,1,3,3))
+  for(j in 2:1){
+    plot.new()
+    text(0.5,0.5, labels=paste0('NMDS', j), srt=ifelse(j == 1, 0, 90))
+  }
+  
+  # ordinations
+  site <- lst_nmds_4[[i]]$points
+  
+  for(j in seq_along(fact_3)){
+    
+    fact <- fact_3[j]
+    
+    env_in_ordi <- env_tot[row.names(env_tot) %in% row.names(site),]
+    
+    pal <- lst_palev[[fact]]$pal[env_in_ordi[[fact]]]
+    
+    #---
+    plot(site, xlim=range(site[,1]), ylim=range(site[,2]), xaxt='n', yaxt='n',
+         col=pal, pch=NA)
+    
+    if(j > 1){
+      axis(1)
+    }
+    
+    if(j %% 2 == 1){
+      axis(2)
+    }
+    
+    ordispider(site, env_in_ordi$plot_in_moist_in_site, col='grey80')
+    
+    ls <- lst_comm[[i]]$clr$env$low_seq[row.names(lst_comm[[i]]$clr$env) %in% row.names(site)]
+    points(site[ls,], pch=19, col=2)
+    
+    text(site, labels=row.names(site), col=pal)
+    
+    abline(v=0, h=0, lty=3)
+    
+  }
+  
+  # legend ---
+  plot.new()
+  
+  text(0.5,0.75,i)
+  
+  # factors colors
+  legend(0.5,0.5, legend=c(unlist(sapply(lst_palev, '[[', 2))), bty='n', xjust=0.5, yjust=0.5,
+         pch=19, col=unlist(sapply(lst_palev, '[[', 1)))
   
 }
 
-file <- paste0(dir_save, '03_lst_comm.Rdata')
-save(lst_comm, file=file)
+dev.off()
+
+# procrustes ---
+lgt <- length(lst_nmds_4)
+
+pvs <- matrix(NA, lgt, lgt, dimnames=list(names(lst_nmds_4),names(lst_nmds_4)))
+
+set.seed(0)
+for(i in 1:(lgt-1)){
+  for(j in (i+1):lgt){
+    pt <- protest(lst_nmds_4[[i]]$points,lst_nmds_4[[j]]$points)
+    pvs[i,j] <- pt$signif
+    pvs[j,i] <- pt$ss
+  }
+}
+
+# on RDA
+pvs <- matrix(NA, lgt, lgt, dimnames=list(names(lst_nmds_4),names(lst_nmds_4)))
+
+set.seed(0)
+for(i in 1:(lgt-1)){
+  for(j in (i+1):lgt){
+    m1 <- lsts$clr[[i]]$lst_ordi$full$site
+    m2 <- lsts$clr[[j]]$lst_ordi$full$site
+    pt <- protest(m1[row.names(m1) %in% smp_4],m2[row.names(m2) %in% smp_4])
+    pvs[i,j] <- pt$signif
+    pvs[j,i] <- pt$ss
+  }
+}
+
+
+
+
+
+
 
 #
 
